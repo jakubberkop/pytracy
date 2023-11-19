b0b3841b1fd7355b6babc51f931b442b9c4bf7e4
Jakub Berkop <jakub.berkop@gmail.com>
2023-11-19 13:36:08 +0100

    Build on windows

--- setup.py
+++ setup.py
@@ -4,10 +4,9 @@ import shutil
 from setuptools import setup, Extension
 from setuptools.command.build_ext import build_ext
 from setuptools.command.install import install
-from subprocess import check_call
+from subprocess import check_call, run
 
-reuse_build_folder = True
-debug = True
+debug = False
 
 class CMakeBuildExt(build_ext):
 	def run(self):
@@ -22,30 +21,38 @@ class CMakeBuildExt(build_ext):
 		# current directory
 		cwd = os.getcwd()
 
+		# Do a clean build every time
+		if os.path.exists(self.build_temp):
+			shutil.rmtree(self.build_temp)
 
-		if not reuse_build_folder:
-			# Do a clean build every time
-			if os.path.exists(self.build_temp):
-				shutil.rmtree(self.build_temp)
+		print("Building Tracy...")
 
-			os.makedirs(self.build_temp)
+		os.makedirs(self.build_temp, exist_ok=True)
 
 		# Change directory to the location where Tracy should be cloned
 		os.chdir(self.build_temp)
 
-		if not reuse_build_folder:
-			# Clone Tracy repository using Git
-			check_call(['git', 'clone', 'https://github.com/wolfpld/tracy'])
+		# Clone Tracy repository using Git
+		check_call(['git', 'clone', 'https://github.com/wolfpld/tracy'])
 
-			# Change directory to Tracy repository
-			os.chdir('tracy')
+		# # Change directory to Tracy repository
+		os.chdir('tracy')
 
-			# Run cmake so that it generates shared libraries
-			# Compile with flags -fPIC -Wl and --no-undefined
-			check_call(['cmake', '.', '-DCMAKE_CXX_FLAGS=-fPIC', '-DCMAKE_BUILD_TYPE=Release'])
+		run(['git', 'checkout', 'v0.10'])
 
-			# Run make to build Tracy
-			check_call(['cmake', '--build', '.', '--config', 'Release'])
+		# Run cmake so that it generates shared libraries
+		# Compile with flags -fPIC -Wl and --no-undefined
+		if os.name == "posix":
+			cxx_flags = "-fPIC"
+		elif os.name == "nt":
+			cxx_flags = ""
+		else:
+			raise Exception("Unsupported OS")
+
+		check_call(['cmake', '.', f'-DCMAKE_CXX_FLAGS={cxx_flags}', '-DCMAKE_BUILD_TYPE=Release'])
+
+		# Run make to build Tracy
+		check_call(['cmake', '--build', '.', '--config', 'Release'])
 
 		self.include_dirs.append(os.path.join(cwd, self.build_temp, 'tracy', 'public', 'tracy'))
 		
@@ -71,11 +78,18 @@ else:
 	extra_link_args = ["-Wno-undef", "-ldl", "-lm"]
 
 if debug:
-	extra_compile_args.extend(["-O0", "-g3"])
-	extra_link_args.extend(["-O0", "-g3"])
+	if os.name == "nt":
+		extra_compile_args.extend(["/Od", "/Zi", "/DEBUG", "/Yd"])
+		extra_link_args.extend(["/DEBUG", "/Zi"])
+	# Linux 
+	elif os.name == "posix":
+		extra_compile_args.extend(["-O0", "-g3"])
+		extra_link_args.extend(["-O0", "-g3"])
 
-	extra_compile_args.extend(["-fsanitize=address", "-fsanitize=undefined"])
-	extra_link_args.extend(["-fsanitize=address", "-fsanitize=undefined"])
+		extra_compile_args.extend(["-fsanitize=address", "-fsanitize=undefined"])
+		extra_link_args.extend(["-fsanitize=address", "-fsanitize=undefined"])
+	else:
+		raise Exception("Unsupported OS")
 
 # Define the custom extension module
 extension = Extension(
