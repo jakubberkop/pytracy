9d441bfd177829315d26a81a8bd7579959ad56f7
Jakub Berkop <jakub.berkop@gmail.com>
2023-11-20 00:39:09 +0100

    Fixed line numbers and cleanup

--- src/pytracy.pyi
+++ src/pytracy.pyi
@@ -2,10 +2,9 @@ from typing import Callable
 from enum import Enum
 
 class TracingMode(Enum):
-	NONE = 0
-	ONLY_SELECTED_FUNCTIONS = 1
-	FULL = 2
+	Disabled = 0
+	MarkedFunctions = 1
+	All = 2
 
 def set_tracing_mode(mode: Enum) -> None: ...
-
 def trace_function(func: Callable) -> Callable: ...
