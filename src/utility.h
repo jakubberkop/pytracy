21c3d201eacdaa167cbff8d76b7a14bd0cefc195
Jakub Berkop <jakub.berkop@gmail.com>
2024-11-15 13:24:09 +0100

    Replace "." with "::" in function names, so that Tracy can divide the name

--- src/utility.h
+++ src/utility.h
@@ -94,3 +94,25 @@ static bool is_path_acceptable(const std::string_view& path, const std::unordere
 
 	return !path_in_excluded_folder(path, filter_list);
 }
+
+std::string replace_all(const std::string& str, const std::string& from, const std::string& to)
+{
+	std::string result;
+	result.reserve(str.size());
+
+	size_t start = 0;
+	size_t end = str.find(from);
+
+	while (end != std::string::npos)
+	{
+		result.append(str, start, end - start);
+		result.append(to);
+
+		start = end + from.size();
+		end = str.find(from, start);
+	}
+
+	result.append(str, start, end);
+
+	return result;
+}
